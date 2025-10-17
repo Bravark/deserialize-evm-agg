0296103dda819afe84d859a5f92104b8fed359a6
xach <sonexzach@gmail.com>
2025-10-17 05:34:34 -0700

    Refactor formatPool method to instantiate route provider for pool formatting

--- providers/routeProviders/AllContructor.ts
+++ providers/routeProviders/AllContructor.ts
@@ -111,7 +111,7 @@ export class AllRoute<DexIdTypes extends string> implements IRoute<any, DexIdTyp
     };
 
     formatPool = (pool: any) => {
-        return AllRoute.formatPool(pool, this.routeProviders);
+        return new this.routeProviders[0](new JsonRpcProvider(), {} as any).formatPool(pool)
     };
 
     listTokens = async () => {
